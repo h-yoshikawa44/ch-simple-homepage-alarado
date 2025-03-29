a31fbe7e209b3280d2681d8e6fac1138f0701b8e
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2025-03-29 23:24:05 +0900

    update: MarkupLintのアプデ 3.15.0 -> 4.11.8 + fix(#13)

--- .markuplintrc.mjs
+++ .markuplintrc.mjs
@@ -1,5 +1,4 @@
-/** @type {import("markuplint").Config} */
-export default {
+/** @type {import("@markuplint/ml-config").Config} */ export default {
   extends: ['markuplint:recommended-static-html'],
   excludeFiles: ['./node_modules/**/*.html'],
 };
